1b899cdb29182beaa395fce612d8ca0cb8265d8d
breannams <breannamarie_s@hotmail.com>
2021-07-12 14:02:16 -0400

    commit tree cbc41a10fc9330b135ba24dfc947421815676a87

--- index.js
+++ index.js
@@ -4,16 +4,30 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (const numbers in collection){
+        callback(collection[numbers])
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let array = []
+      for(const numbers in collection){
+        array.push(callback(collection[numbers]))
+      }
+      return array
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc = 0) {
+      if (acc === 0 ){
+        acc = collection[0]
+        collection = collection.slice(1)
+      }
+      for (let i = 0; i < collection.length; i ++){
+        acc = callback(acc, collection[i], collection)
+      }
+      return acc
     },
 
     functions: function() {
